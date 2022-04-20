04ed6b7fc782773b5162df5daed18a8a50e14dd1
yokesh2411 <103963945+yokesh2411@users.noreply.github.com>
2022-04-20 08:34:16 -0700

    Add files via upload

---
+++ Demo/Mumbai.java
@@ -0,0 +1,17 @@
+package Demo;
+
+public class Mumbai {
+	 void Cityname() {
+		System.out.println("Relocate City name is Dadar");
+	}
+	 protected void Pincode() {
+		 System.out.println("Demo");
+		System.out.println("Pincode is 400014");
+	}
+
+	
+		
+	}
+
+
+
